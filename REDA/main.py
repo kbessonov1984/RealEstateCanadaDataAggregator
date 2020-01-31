a55648adc926d89e0a6cc2e7251ec2b10a5b712c
Kirill Bessonov <kbessonov@gmail.com>
2020-01-31 12:20:09 -0500

    encodings parameters for html files with unicode characters

--- REDA/main.py
+++ REDA/main.py
@@ -15,13 +15,13 @@ class Home:
         self.datetoday = ""
         self.latitude = ""
         self.longitude = ""
-        self.neighbourhood = "",
-        self.age = "",
-        self.size = "",
-        self.taxes = "",
-        self.daysonmarket = "",
-        self.soldprice = "",
-        self.status = "",
+        self.neighbourhood = "-"
+        self.age = ""
+        self.size = ""
+        self.taxes = ""
+        self.daysonmarket = ""
+        self.soldprice = ""
+        self.status = ""
 
 
 
@@ -370,7 +370,7 @@ if __name__ == '__main__':
     # here, we fetch the content from the url, using the requests library
     #page_content = BeautifulSoup(page_response.content, "html.parser")
     if os.path.exists("page.html"):
-        with open(file="page.html", mode="r") as fp:
+        with open(file="page.html", mode="r", encoding = "utf-8") as fp:
             page_html = fp.read()
         html_soup = BeautifulSoup(page_html, "html.parser")
         house_containers = html_soup.find_all('div', class_="smallListingCard")  # from the realtor.hmtl file
@@ -380,7 +380,7 @@ if __name__ == '__main__':
 
 
     for house_container in house_containers:
-        MLSvaluePrevious = getPreviousRecordsFromFile()
+        MLSvaluePrevious = getPreviousRecordsFromFile() #dictionary of previous MLS values
 
         MLSvalue = house_container.find_all("div", class_="smallListingCardMLSVal")[0]["title"]
 
@@ -390,6 +390,7 @@ if __name__ == '__main__':
 
 
         HomeInstance = Home()
+
 
 
         HomeInstance.realtorurl = str(house_container.find_all("a",class_="blockLink listingDetailsLink")[0]["href"])
@@ -418,7 +419,10 @@ if __name__ == '__main__':
 
         HomeInstance.datetoday = datetime.date.today()
 
+
+        #put meta data from Zolo by MLS and address
         ZoloMetaDict = ZoloMetaDataPull(MLSvalue=MLSvalue, header=header, address=HomeInstance.address)
+
         if HomeInstance.type == "Unknown" and ZoloMetaDict["HomeType"] != "-":
             HomeInstance.type=ZoloMetaDict["HomeType"]
 
@@ -437,11 +441,11 @@ if __name__ == '__main__':
         dictHomeObjects[MLSvalue]= HomeInstance
 
 
-    MLSCityZoloDict = getZoloRecodsFromNet(city = "guelph")
+    #get other listings data
+    MLSCityZoloDict = getZoloRecodsFromNet(city = "Guelph")
 
     for MLSvalue in MLSCityZoloDict.keys():
-        MLSvaluePrevious = list(getPreviousRecordsFromFile().keys()) + list(dictHomeObjects.keys())
-
+        MLSvaluePrevious = list(getPreviousRecordsFromFile().keys()) + list(dictHomeObjects.keys()) #previous MLS values
 
         if MLSvaluePrevious:
             if MLSvalue in MLSvaluePrevious:
